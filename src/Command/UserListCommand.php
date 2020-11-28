92987a58f40614d45613601824f9a224bdb3b0f4
Ambroz <n7komandore@gmail.com>
2020-11-28 14:52:30 +0100

    generowanie danych w konsoli

--- src/Command/UserListCommand.php
+++ src/Command/UserListCommand.php
@@ -9,17 +9,19 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Faker\Factory;
+use Symfony\Component\Console\Helper\Table;
 
 class UserListCommand extends Command
 {
     protected static $defaultName = 'user:list';
     protected $faker;
+    private $arr;
 
     protected function configure()
     {
         $this
             ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addArgument('cases', InputArgument::REQUIRED, 'liczba wygenerowanych danych')
 //            ->addOption('randomize', '-r', InputOption::VALUE_NONE, 'Option description')
         ;
     }
@@ -31,16 +33,23 @@ class UserListCommand extends Command
         $this->faker = Factory::create();
 
         if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+
             for($i=0; $i<$arg1; $i++ ){
-                echo $this->faker->name;
-                echo $this->faker->email;
-                echo $this->faker->address;
+                $this->arr[] = [$this->faker->firstName, $this->faker->lastName, $this->faker->address];
             }
+
+            $table = new Table($output);
+            $table
+                ->setHeaders(['First Name', 'Last Name', 'Address'])
+                ->setRows($this->arr);
+            $table->render();
+
+            echo "Json:\n";
+            $json = json_encode($this->arr);
+            echo $json;
         }
 
-
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $io->success('Zakończono generowanie danych');
 
         return Command::SUCCESS;
     }
